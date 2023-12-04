955ddbd0fb5ab66f2fd73cfc55f68f6c6a2d68e2
HubardShakita <132462389+HubardShakita@users.noreply.github.com>
2023-12-04 21:20:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function fetchData() {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let responseData = JSON.parse(xhr.responseText);
+                let imageUrl = responseData.url;
+                let imageContainer = document.querySelector('.container');
+                if (imageContainer.firstChild) {
+                    imageContainer.removeChild(imageContainer.firstChild);
+                }
+                let image = document.createElement('img');
+                image.src = imageUrl;
+                imageContainer.appendChild(image);
+            }
+            else {
+                prompt('Произошла ошибка при получении данных');
+            }
+        }
+    };
+
+    xhr.open('GET', 'https://random.dog/woof.json', true);
+    xhr.send();
+}
+
+fetchData();
